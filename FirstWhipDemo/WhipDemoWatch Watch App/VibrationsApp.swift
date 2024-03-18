582d337f5676504e22311eb1f61f6f989c7c1fa9
Antonio <147817129+AntonioTri@users.noreply.github.com>
2024-03-18 10:15:09 +0100

    Adding Working Version 1

---
+++ FirstWhipDemo/WhipDemoWatch Watch App/VibrationsApp.swift
@@ -0,0 +1,19 @@
+//
+//  VibrationsApp.swift
+//  Vibrations Watch App
+//
+//  Created by Antonio Tridente on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Vibrations_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
